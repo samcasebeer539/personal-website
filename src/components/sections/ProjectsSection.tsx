6274bf8472e666d9ed457fdc751da2dc14bba733
samcasebeer539 <samuel.casebeer@gmail.com>
2026-05-18 20:16:10 -0700

    surely this time

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -98,7 +98,7 @@ export default function ProjectsSection() {
               </div>
               <div className="flex flex-wrap gap-2 mt-3 mb-3">
                 {photos.map((filename) => (
-                  <div key={filename} className="overflow-hidden rounded-xs w-[calc(20%-0.6rem)] min-w-40">
+                  <div key={filename} className="overflow-hidden rounded-xs w-[calc(20%-0.6rem)] min-w-20">
                     <img
                       src={`/winwinphotos/${filename}`}
                       alt={filename}
